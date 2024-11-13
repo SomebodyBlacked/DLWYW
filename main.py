f7adfa1e4e2bf75685b30cf1b0693096b675a4de
Alguien <WhiteComment@protonmail.com>
2024-11-13 04:00:59 +0000

    change error message

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pytubefix import YouTube
 import sys
 
 if len(sys.argv) < 2:
-  print("Usage: python main.py <YouTube URL>")
+  print("Error: Please provide a YouTube URL as an argument.")
   sys.exit(1)
 
 url = sys.argv[1]
